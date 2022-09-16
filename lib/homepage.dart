7efffa2e869bc613f78b2919b3653d6422b80d25
Arjun <j.arjunannamalai@gmail.com>
2022-09-16 15:19:27 +0530

    appicon

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -16,46 +16,62 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(
-          "Homepage",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 28,
-          ),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: IconButton(
-                onPressed: () async {
-                  await FirebaseServices().SignOutGoogle();
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Login()));
-                },
-                icon: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                  size: 28,
-                )),
-          )
-        ],
-        centerTitle: false,
         backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          children: <Widget>[
-            Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
-            Text("${FirebaseAuth.instance.currentUser!.displayName}"),
-            Text("${FirebaseAuth.instance.currentUser!.email}"),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text(
+            "Homepage",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                  onPressed: () async {
+                    await FirebaseServices().SignOutGoogle();
+                    Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => Login()),
+                          (route) => false);
+                  },
+                  icon: Icon(
+                    Icons.logout,
+                    color: Colors.black,
+                    size: 28,
+                  )),
+            )
           ],
+          centerTitle: false,
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-      )),
-    );
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Name: ${FirebaseAuth.instance.currentUser!.displayName}",
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "email id: ${FirebaseAuth.instance.currentUser!.email}",
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        )));
   }
 }
